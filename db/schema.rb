eae78aa906ea1612dd365213c5bbcb350fbda056
Sydney Young <sydney.young@yale.edu>
2016-11-21 15:31:49 -0500

    Add ability to edit Users
    Resolves #35
      - Add name attributes to Users
      - Add name helpers to Users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161121164737) do
+ActiveRecord::Schema.define(version: 20161121195134) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -68,6 +68,10 @@ ActiveRecord::Schema.define(version: 20161121164737) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.integer  "role",                   default: 0,  null: false
+    t.string   "first_name",                          null: false
+    t.string   "last_name",                           null: false
+    t.string   "preferred_name"
+    t.string   "middle_name"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
---
+++ spec/features/users/create_spec.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+require 'rails_helper'
+
+RSpec.feature 'User Promotion' do
+  before { log_in FactoryGirl.create(:admin) }
+  it 'can be performed' do
+    user = FactoryGirl.create(:user)
+    visit edit_user_path(user)
+    select('rep', from: 'user_role')
+    click_on 'Save'
+    expect(page).to have_css('.user-role', text: 'rep')
+  end
+end
