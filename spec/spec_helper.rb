81f3f627d7c5ad5627d8d24757195e9160418b0b
Oren Kanner <okanner@gmail.com>
2016-12-07 23:18:18 -0500

    Add CodeClimate test coverage reporting
    Resolves #62
    - Remove some code from Suspenders related to CircleCI

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,16 +1,8 @@
 # frozen_string_literal: true
-if ENV.fetch('COVERAGE', false)
-  require 'simplecov'
-
-  if ENV['CIRCLE_ARTIFACTS']
-    dir = File.join(ENV['CIRCLE_ARTIFACTS'], 'coverage')
-    SimpleCov.coverage_dir(dir)
-  end
-
-  SimpleCov.start 'rails'
-end
-
+require 'simplecov'
 require 'webmock/rspec'
+
+SimpleCov.start
 
 # http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
