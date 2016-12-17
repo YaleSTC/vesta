9a928c6be60440f7b61e6ed28b16478cd434d4ce
Oren Kanner <okanner@gmail.com>
2016-12-16 19:54:30 -0500

    Fix order of resource before actions
    Resolves #63
    - Change `before_action` to `prepend_before_action` in facilities
      controllers (building, suite, and room). This ensures that the
      specific resources are loaded into the relevant instance variables
      before Pundit checks for authorization to interact with those records

--- app/controllers/suites_controller.rb
+++ app/controllers/suites_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 # Controller for Suites
 class SuitesController < ApplicationController
-  before_action :set_suite, only: %i(show edit update destroy)
+  prepend_before_action :set_suite, only: %i(show edit update destroy)
 
   def show
   end
