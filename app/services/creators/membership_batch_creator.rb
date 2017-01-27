898e0623c2c08a6dfc7153b9e662acdde5b15360
Sydney Young <sydney.young@yale.edu>
2017-01-27 12:29:22 -0500

    Add Group invitations
    Resolves #90
     - Adds MembershipBatchCreator service object

---
+++ app/services/creators/membership_batch_creator.rb
@@ -0,0 +1,74 @@
+# frozen_string_literal: true
+#
+# Class to create memberships for many users with the same attributes
+class MembershipBatchCreator
+  def self.run(**params)
+    new(params).run
+  end
+
+  # Create a new MembershipBatchCreator
+  #
+  # @param [Array] user_ids The ids of users to create memberships for
+  # @param [Group] group The group to create memberships in
+  # @param [#to_h] params The params for the memberships
+  def initialize(user_ids:, group:, **params)
+    @users = User.find(user_ids.reject(&:empty?))
+    @params = params
+    @group = group
+  end
+
+  # Use MembershipCreator to create many memberships. Creates no memberships if
+  # creating all of them would exceed the size.
+  #
+  # @return [Hash{Symbol=>Array,Hash}] A hash with the group and draw to
+  #   redirect to and a flash message with the results of the batch create
+  def run
+    return error if too_many_invitations?
+    @results = users.map do |u|
+      MembershipCreator.create!(params.to_h.merge(user: u, group: group))
+    end
+    build_result
+  end
+
+  private
+
+  attr_reader :users, :successes, :failures, :results, :params, :group
+
+  def error
+    { object: nil, msg: { error: 'Too many invitations' } }
+  end
+
+  def too_many_invitations?
+    (group.memberships_count + users.count) > group.size
+  end
+
+  def build_result
+    @successes, @failures = results.partition { |r| r[:msg].key? :success }
+    { object: [group.draw, group], msg: build_flash }
+  end
+
+  def build_flash
+    if successes.empty?
+      { error: failure_msg }
+    elsif failures.empty?
+      { success: success_msg }
+    else
+      { success: success_msg, error: failure_msg }
+    end
+  end
+
+  def success_msg
+    return if successes.empty?
+    @success_msg ||= 'Successfully created memberships for '\
+      "#{successes.map { |s| s[:membership].user.full_name }.join(', ')}"
+  end
+
+  def failure_msg
+    return if failures.empty?
+    @failure_errors ||= failures.map do |f|
+      "#{f[:params][:user].full_name} (#{f[:errors].join(', ')})"
+    end
+    @failure_msg ||= 'Failed to create memberships for '\
+      "\n#{@failure_errors.join('\n')}"
+  end
+end
--- spec/services/creators/membership_creator_spec.rb
+++ spec/services/creators/membership_creator_spec.rb
@@ -9,7 +9,14 @@ RSpec.describe MembershipCreator do
       described_class.create!(params)
       expect(membership_creator).to have_received(:create!)
     end
+
+    def mock_membership_creator(param_hash)
+      instance_spy('MembershipCreator').tap do |mc|
+        allow(described_class).to receive(:new).with(param_hash).and_return(mc)
+      end
+    end
   end
+
   context 'success' do
     it 'creates a membership object' do
       params = instance_spy('ActionController::Parameters', to_h: params_hash)
@@ -21,25 +28,19 @@ RSpec.describe MembershipCreator do
       params = instance_spy('ActionController::Parameters', to_h: params_hash)
       expect(described_class.new(params).create![:msg]).to have_key(:success)
     end
+
+    # rubocop:disable RSpec/InstanceVariable
+    def params_hash
+      @group ||= FactoryGirl.create(:open_group)
+      @user ||= FactoryGirl.build_stubbed(:student, intent: 'on_campus',
+                                                    draw: @group.draw)
+      { group: @group, user: @user, status: 'requested' }
+    end
+    # rubocop:enable RSpec/InstanceVariable
   end
 
   it 'does not create when given invalid params' do
     params = instance_spy('ActionController::Parameters', to_h: {})
     expect(described_class.new(params).create![:object]).to be_nil
   end
-
-  def mock_membership_creator(param_hash)
-    instance_spy('MembershipCreator').tap do |mc|
-      allow(described_class).to receive(:new).with(param_hash).and_return(mc)
-    end
-  end
-
-  # rubocop:disable RSpec/InstanceVariable
-  def params_hash
-    @group ||= FactoryGirl.create(:open_group)
-    @user ||= FactoryGirl.build_stubbed(:student, intent: 'on_campus',
-                                                  draw: @group.draw)
-    { group: @group, user: @user, status: 'requested' }
-  end
-  # rubocop:enable RSpec/InstanceVariable
 end
