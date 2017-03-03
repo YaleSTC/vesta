7fd9198175f13874640ed2351607665046eb177f
Sydney Young <sydney.young@yale.edu>
2017-03-03 07:01:37 -0500

    Update Building Suite Display
    Resolves #212
    - Improve building index and show views
    - Add Building#suites_by_size (to be extracted into query object)

--- spec/models/building_spec.rb
+++ spec/models/building_spec.rb
@@ -8,4 +8,8 @@ RSpec.describe Building, type: :model do
     it { is_expected.to validate_uniqueness_of(:name) }
     it { is_expected.to have_many(:suites) }
   end
+
+  describe '#suites_by_size' do
+    xit 'it retrieves all the suites by size in a given building'
+  end
 end
