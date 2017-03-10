5945094ca54c91f8951d62a6d76f75ce47c7e199
Oren Kanner <okanner@gmail.com>
2017-03-10 10:51:09 -0500

    Set up Mail config to support optional authentication
    Resolves #351
    - Add documentation of additional parameters to .env.example
    - Mailer delivery errors will now raise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+# rubocop:disable BlockLength
 require Rails.root.join('config/smtp')
 Rails.application.configure do
   if ENV.fetch('HEROKU_APP_NAME', '').include?('staging-pr-')
@@ -16,6 +17,7 @@ Rails.application.configure do
                                                   ENV.fetch('APPLICATION_HOST'))
   config.log_level = :debug
   config.log_tags = [:request_id]
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = SMTP_SETTINGS
