dd98ff4a790cd362c46d8c62056a3bbc197d209d
Oren Kanner <okanner@gmail.com>
2017-03-10 10:51:09 -0500

    Set up Mail config to support optional authentication
    Resolves #351
    - Add documentation of additional parameters to .env.example
    - Mailer delivery errors will now raise

--- config/smtp.rb
+++ config/smtp.rb
@@ -1,13 +1,20 @@
 # frozen_string_literal: true
+auth_settings = if ENV['SMTP_AUTH'].present?
+                  {
+                    authentication: ENV.fetch('SMTP_AUTH').to_sym,
+                    user_name: ENV.fetch('SMTP_USERNAME'),
+                    password: ENV.fetch('SMTP_PASSWORD')
+                  }
+                else
+                  {}
+                end
+
 SMTP_SETTINGS = {
   address: ENV.fetch('SMTP_ADDRESS'), # example: "smtp.sendgrid.net"
-  authentication: :plain,
   domain: ENV.fetch('SMTP_DOMAIN'), # example: "heroku.com"
   enable_starttls_auto: true,
-  password: ENV.fetch('SMTP_PASSWORD'),
-  port: '587',
-  user_name: ENV.fetch('SMTP_USERNAME')
-}.freeze
+  port: ENV.fetch('SMTP_PORT')
+}.merge(auth_settings).freeze
 
 if ENV['EMAIL_RECIPIENTS'].present?
   Mail.register_interceptor RecipientInterceptor.new(ENV['EMAIL_RECIPIENTS'])
