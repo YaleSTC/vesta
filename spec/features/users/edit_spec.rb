b4c21a81bbb8c9e1b48cfd200ffcbe8b8345ac4a
Oren Kanner <okanner@gmail.com>
2017-03-11 19:51:59 -0500

    Allow admins to edit users; reorganize profile pages
    Resolves #353
    - Add profile fields (name / email) back to the user edit form
    - Add Edit button to user profiles
    - Add draw / e-mail to User profiles and reorganize the view
    - Add UserPolicy#draw_info?

--- spec/features/users/edit_spec.rb
+++ spec/features/users/edit_spec.rb
@@ -5,9 +5,26 @@ RSpec.feature 'User Editing' do
   let(:user) { FactoryGirl.create(:student) }
   before { log_in FactoryGirl.create(:admin) }
   it 'can update role' do
-    visit edit_user_path(user)
+    visit_edit_form(user)
     select('rep', from: 'user_role')
     click_on 'Save'
     expect(page).to have_css('.user-role', text: 'rep')
+  end
+  it 'can update first name' do
+    visit_edit_form(user)
+    fill_in 'First name', with: 'Captain'
+    click_on 'Save'
+    expect(page).to have_css('.user-name', text: /Captain/)
+  end
+  it 'can update email' do
+    visit_edit_form(user)
+    fill_in 'Email', with: 'foo@foo.com'
+    click_on 'Save'
+    expect(page).to have_css('.user-email', text: 'foo@foo.com')
+  end
+
+  def visit_edit_form(user)
+    visit user_path(user)
+    click_on 'Edit'
   end
 end
