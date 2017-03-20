df7f72ca8c24664634b4ac86f3b0fcc7caf790f3
Oren Kanner <okanner@gmail.com>
2017-03-20 00:43:36 -0400

    Improve user index
    Resolves #297
    - Add users/table and users/actions partials
    - Add UsersController#destroy with feature specs

---
+++ spec/features/users/user_deletion_spec.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+require 'rails_helper'
+
+RSpec.feature 'User deletion' do
+  before { log_in FactoryGirl.create(:admin) }
+  let!(:user) { FactoryGirl.create(:user) }
+
+  it 'succeeds' do
+    msg = "User #{user.full_name} deleted."
+    visit users_path
+    within("tr#user-#{user.id}") { click_on 'Remove' }
+    expect(page).to have_content(msg)
+  end
+end
