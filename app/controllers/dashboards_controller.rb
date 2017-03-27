7c85ee9f9acd8c483b8d85de03bd9d46d0942d6d
Sydney Young <sydney.young@yale.edu>
2017-03-26 22:00:26 -0400

    Add dashboards
    Resolves #303

---
+++ app/controllers/dashboards_controller.rb
@@ -0,0 +1,77 @@
+# frozen_string_literal: true
+# Controller for Dashboards
+class DashboardsController < ApplicationController
+  before_action :authorize!
+
+  def show
+    admin_metrics unless current_user.student?
+    student_variables unless current_user.admin?
+  end
+
+  private
+
+  def admin_metrics
+    @draws = Draw.all.includes(:groups).sort_by(&:name)
+    @draw_hash = Hash.new { |hash, key| hash[key] = {} }
+    @draws.each do |d|
+      @draw_hash[d][:partial] = d.status
+      if %w(pre_lottery suite_selection results).include? d.status
+        send("#{d.status}_metrics".to_sym, d, @draw_hash[d])
+      end
+    end
+  end
+
+  def pre_lottery_metrics(draw, hash) # rubocop:disable MethodLength, AbcSize
+    suite_counts = draw.suites.available.group(:size).count
+    suite_counts.default = 0
+    groups = draw.groups.sort_by { |g| Group.statuses[g.status] }
+                 .group_by(&:size)
+    groups.default = []
+    group_counts = groups.transform_values(&:count)
+    group_counts.default = 0
+    sizes = (suite_counts.keys + group_counts.keys).uniq.sort
+    diff = sizes.map { |s| [s, suite_counts[s] - group_counts[s]] }.to_h
+
+    locked_counts = groups.transform_values { |v| v.select(&:locked?).size }
+    locked_counts.default = 0
+
+    hash[:suite_counts] = suite_counts
+    hash[:group_counts] = group_counts
+    hash[:locked_counts] = locked_counts
+    hash[:groups_by_size] = groups
+    hash[:sizes] = sizes
+    hash[:diff] = diff
+
+    hash[:intent] = IntentMetricsQuery.call(draw)
+  end
+
+  def suite_selection_metrics(draw, hash)
+    with_suites = draw.groups.joins(:suite)
+
+    with_suite_count = with_suites.count
+    no_suite_count = draw.groups.count - with_suite_count
+    no_room_count = with_suite_count - with_suites.joins(leader: :room).count
+
+    hash[:next_groups] = draw.next_groups
+    hash[:no_suite_count] = no_suite_count
+    hash[:with_suite_count] = with_suite_count
+    hash[:no_room_count] = no_room_count
+  end
+
+  def results_metrics(draw, hash)
+    with_suites = draw.groups.joins(:suite)
+    no_room_count = with_suites.count - with_suites.joins(leader: :room).count
+    hash[:no_room_count] = no_room_count
+  end
+
+  def student_variables
+    @college = College.first || College.new
+    group = current_user.group
+    @suite = group.suite if group.present?
+    @room = current_user.room if @suite.present?
+  end
+
+  def authorize!
+    authorize Dashboard
+  end
+end
---
+++ spec/features/dashboards/dashboards_spec.rb
@@ -0,0 +1,54 @@
+# frozen_string_literal: true
+require 'rails_helper'
+
+RSpec.feature 'Dashboards' do
+  context 'admin' do
+    it 'renders' do
+      create_draws
+      log_in FactoryGirl.create(:admin)
+      expect(page).to have_content('Vesta')
+    end
+
+    def create_draws
+      FactoryGirl.create(:draw_with_members, status: 'draft')
+      FactoryGirl.create(:oversubscribed_draw, status: 'pre_lottery')
+      FactoryGirl.create(:draw_in_lottery)
+      FactoryGirl.create(:draw_in_selection)
+      # TODO: results draw
+    end
+  end
+
+  context 'student' do
+    shared_examples 'renders' do
+      it do
+        log_in student
+        expect(page).to have_content('Vesta')
+      end
+    end
+    context 'without group' do
+      it_behaves_like 'renders' do
+        let(:student) { FactoryGirl.create(:student_in_draw) }
+      end
+    end
+    context 'with group no suite' do
+      it_behaves_like 'renders' do
+        let(:student) { FactoryGirl.create(:group).leader }
+      end
+    end
+    context 'with suite no room' do
+      it_behaves_like 'renders' do
+        let(:student) { FactoryGirl.create(:full_group, :with_suite).leader }
+      end
+    end
+    context 'with room' do
+      it_behaves_like 'renders' do
+        let(:student) do
+          g = FactoryGirl.create(:full_group, :with_suite)
+          s = g.leader
+          s.update(room: g.suite.rooms.first)
+          s
+        end
+      end
+    end
+  end
+end
