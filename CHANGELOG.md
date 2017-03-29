61c39b3dd04950edabcb63600f088b6ae1efa01d
Sydney Young <sydney.young@yale.edu>
2017-03-29 12:57:56 -0400

    Prevent students from selecting suites
    Resolves #501

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,9 @@
 All notable changes to this project will be documented in this file.
 
 ## Unreleased
+### Fixed
+* Prevent students from performing suite selection ([#501](https://github.com/YaleSTC/vesta/issues/501)).
+
 ### Changed
 * Downgrade Ruby to v2.3.3 ([#493](https://github.com/YaleSTC/vesta/issues/493)).
 
--- spec/features/groups/suite_selection_spec.rb
+++ spec/features/groups/suite_selection_spec.rb
@@ -1,22 +1,51 @@
 # frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.feature 'Suite Selection' do
-  let(:leader) do
-    FactoryGirl.create(:draw_in_selection).next_groups.first.leader.tap do |l|
-      l.update(password: 'password')
+  context 'valid' do
+    let(:leader) do
+      FactoryGirl.create(:draw_in_selection).next_groups.first.leader.tap do |l|
+        l.update(password: 'password')
+      end
+    end
+
+    # not running because this was disabled for pilot
+    xit 'can be performed by group leaders' do
+      suite = leader.draw.suites.where(size: leader.group.size).first
+      log_in leader
+      select_suite(suite.number)
+      expect(page).to have_content("#{suite.number} assigned")
+    end
+
+    def select_suite(number)
+      click_on 'Select Suite'
+      choose number
+      click_on 'Submit Selection'
     end
   end
-  it 'can be performed by group leaders' do
-    suite = leader.draw.suites.where(size: leader.group.size).first
-    log_in leader
-    select_suite(suite.number)
-    expect(page).to have_content("#{suite.number} assigned")
+
+  context 'draw not in suite selection' do
+    let(:leader) do
+      FactoryGirl.create(:open_group).leader.tap do |l|
+        l.update(password: 'password')
+        l.reload
+      end
+    end
+
+    it 'link does not show' do
+      log_in leader
+      expect(page).not_to have_content('Select Suite')
+    end
+
+    it 'cannot reach page' do
+      log_in leader
+      visit select_suite_draw_group_path(leader.draw, leader.group)
+      expect(page).to have_content("don't have permission")
+    end
   end
 
-  def select_suite(number)
-    click_on 'Select Suite'
-    choose number
-    click_on 'Submit Selection'
+  context 'not next group' do
+    xit 'WRITE'
   end
 end
