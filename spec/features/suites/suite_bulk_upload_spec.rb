25e9b3601c7b213a02609b58063ce0051441f141
Melina <melina.delgado@yale.edu>
2017-04-17 17:40:31 -0400

    Prevent non-admins from seeing the suite import form.
    Resolves #576

--- spec/features/suites/suite_bulk_upload_spec.rb
+++ spec/features/suites/suite_bulk_upload_spec.rb
@@ -1,14 +1,26 @@
 # frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.feature 'Suite CSV Import' do
-  before { log_in FactoryGirl.create(:admin) }
-  it 'succeeds' do # rubocop:disable RSpec/ExampleLength
-    building = FactoryGirl.create(:building)
-    visit building_path(building)
-    attach_file('suite_import_form[file]',
-                "#{Rails.root}/spec/fixtures/suite_upload.csv")
-    click_on 'Import'
-    expect(building.suites.count).to eq(2)
+  context 'admin' do
+    before { log_in FactoryGirl.create(:admin) }
+    it 'succeeds' do # rubocop:disable RSpec/ExampleLength
+      building = FactoryGirl.create(:building)
+      visit building_path(building)
+      attach_file('suite_import_form[file]',
+                  Rails.root.join('spec', 'fixtures', 'suite_upload.csv'))
+      click_on 'Import'
+      expect(building.suites.count).to eq(2)
+    end
+  end
+
+  context 'student' do
+    before { log_in FactoryGirl.create(:student_in_draw) }
+    it 'does not show suite_import_form' do
+      building = FactoryGirl.create(:building)
+      visit building_path(building)
+      expect(page).not_to have_content('Import')
+    end
   end
 end
