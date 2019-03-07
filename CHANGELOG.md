5ef0fcb477ebc46ba80b93fbd69bae5f71ccc25f
Ashvin Pradhan-Shenoy <ashvin.pradhan-shenoy@yale.edu>
2019-03-07 10:17:20 -0500

    Remove stale DrawMembership records.
    Resolves #933
     - Removes DrawMembership records associated with neither Draws or
    Memberships.
     - Removes DrawMembership records that are inactive and associated with
    active draws.
     - Will raise ActiveRecord::IrreversibleMigration

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -14,6 +14,7 @@ All notable changes to this project will be documented in this file.
 * Rename group size locking to restrict size locking ([811](https://gitlab.com/yale-sdmp/vesta/issues/811)).
 * Association of draw to draw_membership has been changed to have matching "active" states. ([#928](https://gitlab.com/yale-sdmp/vesta/issues/928)).
 * Remove code from drawStudentsUpdate to manually add students by username ([900](https://gitlab.com/yale-sdmp/vesta/issues/900)).
+* Remove stale records of DrawMemberships ([933](https://gitlab.com/yale-sdmp/vesta/issues/933)).
 
 ## v2.2.1 - 2019-02-20
 ### Added
---
+++ db/migrate/20190307151207_remove_orphan_draw_memberships.rb
@@ -0,0 +1,26 @@
+class RemoveOrphanDrawMemberships < ActiveRecord::Migration[5.1]
+  def up
+    # Remove inactive draw_membership records that are associated with active
+    # draws.
+    execute <<-SQL
+      DELETE FROM draw_memberships
+      USING  draws
+      WHERE  draw_id = draws.id
+             AND draw_memberships.active = FALSE
+             AND draws.active = TRUE
+    SQL
+
+    # Remove draw_membership records that are not associated with a membership
+    # OR a draw.
+    execute <<-SQL
+      DELETE FROM draw_memberships
+      USING  memberships
+      WHERE  draw_memberships.id != memberships.draw_membership_id
+             AND draw_memberships.draw_id IS NULL
+    SQL
+
+    def down
+      raise ActiveRecord::IrreversibleMigration
+    end
+  end
+end
