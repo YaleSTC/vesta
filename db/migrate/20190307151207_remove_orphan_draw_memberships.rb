b8c32057ace54c83a76f536b9adddd2cea1bac2c
Oren Kanner <okanner@gmail.com>
2019-03-20 11:19:53 -0400

    Partially revert cleanup migration

--- db/migrate/20190307151207_remove_orphan_draw_memberships.rb
+++ db/migrate/20190307151207_remove_orphan_draw_memberships.rb
@@ -12,12 +12,12 @@ class RemoveOrphanDrawMemberships < ActiveRecord::Migration[5.1]
 
     # Remove draw_membership records that are not associated with a membership
     # OR a draw.
-    execute <<-SQL
-      DELETE FROM draw_memberships
-      USING  memberships
-      WHERE  draw_memberships.id != memberships.draw_membership_id
-             AND draw_memberships.draw_id IS NULL
-    SQL
+    # execute <<-SQL
+    #   DELETE FROM draw_memberships
+    #   USING  memberships
+    #   WHERE  draw_memberships.id != memberships.draw_membership_id
+    #          AND draw_memberships.draw_id IS NULL
+    # SQL
 
     def down
       raise ActiveRecord::IrreversibleMigration
