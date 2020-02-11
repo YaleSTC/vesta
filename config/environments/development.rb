6dcfc976c1d0ca3f434491daa2a9ba2dab2165a2
Matt Coshal <mattcosh@gmail.com>
2020-02-11 10:24:39 -0500

    Remove listen gem config
    Resolves #1044

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
+
+# rubocop:disable Metrics/BlockLength
 Rails.application.configure do
   config.cache_classes = false
   config.eager_load = false
   config.consider_all_requests_local = true
-  if Rails.root.join('tmp/caching-dev.txt').exist?
+  if Rails.root.join('tmp', 'caching-dev.txt').exist?
     config.action_controller.perform_caching = true
     config.cache_store = :memory_store
     config.public_file_server.headers = {
@@ -26,6 +28,5 @@ Rails.application.configure do
   config.assets.debug = true
   config.assets.quiet = true
   config.action_view.raise_on_missing_translations = true
-  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
 end
